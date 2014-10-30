b72fc1664eb0b5671f376b72950eba162c82b7dc
Sourabh Bajaj <sb2nov@gmail.com>
2014-10-30 00:42:16 -0700

    Initial Commit

---
+++ CHANGES.md
@@ -0,0 +1,6 @@
+Changes in ETL_Lib
+===================
+
+0.0.1
+=====
+* Starting the Project
---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+Setup file for installation of the etllib code
+"""
+from distutils.core import setup
+
+setup(
+    name='etllib',
+    version='0.0.1',
+    author='Coursera Inc.',
+    packages=['etllib'],
+    namespace_packages=['etllib'],
+    include_package_data=True,
+    url='http://www.coursera.org',
+    long_description=open('README.rst').read(),
+    author_email='data-infra@coursera.org',
+    license='BSD License',
+    description='Fix everything about datapipeline',
+    install_requires=[
+        "setuptools",
+    ],
+    classifiers=[
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
+
+# TODO: change name
+# TODO: change description
+# TODO: change dependencies
+# TODO: Update packages
+# TODO: change version fix
+# TODO: change url from documentation
