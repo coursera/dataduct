64b4c80bcfd173ad997692c5baf5bf12e39120ca
Sourabh Bajaj <sb2nov@gmail.com>
2014-11-08 03:22:39 -0800

    emr activity

--- etllib/pipeline/emr_activity.py
+++ etllib/pipeline/emr_activity.py
@@ -1,40 +1,35 @@
 """
-Pipeline object class for SqlActivity
+Pipeline object class for EmrActivity
 """
 
 from .activity import Activity
 from .schedule import Schedule
-from ..s3.s3_file import S3File
-
 from ..constants import DEFAULT_MAX_RETRIES
-from ..constants import RETRY_DELAY
 from ..utils.exceptions import ETLInputError
 
 
-class SqlActivity(Activity):
-    """Sql Activity class
+class EmrActivity(Activity):
+    """EMR Activity class
     """
 
     def __init__(self,
                  id,
                  resource,
                  schedule,
-                 script,
-                 database,
-                 script_arguments=None,
-                 queue=None,
+                 emr_step_string,
+                 output_node=None,
+                 additional_files=None,
                  max_retries=None,
                  depends_on=None):
-        """Constructor for the SqlActivity class
+        """Constructor for the EmrActivity class
 
         Args:
             id(str): id of the object
             resource(Ec2Resource / EMRResource): resource to run the activity on
             schedule(Schedule): schedule of the pipeline
-            script(S3File): s3 uri of the script
-            script_arguments(list of str): command line arguments to the script
-            database(RedshiftDatabase): database to execute commands on
-            queue(str): queue in which the query should be executed
+            emr_step_string(list of str): command string to be executed
+            output_node(S3Node): output_node for the emr job
+            additional_files(list of S3File): Additional files required for emr
             max_retries(int): number of retries for the activity
             depends_on(list of activities): dependendent pipelines steps
         """
@@ -44,25 +39,21 @@ class SqlActivity(Activity):
             raise ETLInputError(
                 'Input schedule must be of the type Schedule')
 
-        if not isinstance(script, S3File):
-            raise ETLInputError('script must be an S3File')
-
         # Set default values
         if depends_on is None:
             depends_on = []
         if max_retries is None:
             max_retries = DEFAULT_MAX_RETRIES
 
-        super(SqlActivity, self).__init__(
+        super(EmrActivity, self).__init__(
             id=id,
-            retryDelay=RETRY_DELAY,
-            type='SqlActivity',
+            type='EmrActivity',
             maximumRetries=max_retries,
             dependsOn=depends_on,
             runsOn=resource,
             schedule=schedule,
-            scriptUri=script,
-            scriptArgument=script_arguments,
-            database=database,
-            queue=queue
+            step=emr_step_string,
+            output=output_node,
         )
+
+        self.add_additional_files(additional_files)
