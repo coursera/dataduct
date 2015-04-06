7e4902d3be35bda869badd4819f5c93f9271a032
Jerry Jiang <jjiang@coursera.org>
2015-04-06 14:29:19 -0700

    Add tests for parse_path

--- dataduct/utils/tests/test_helpers.py
+++ dataduct/utils/tests/test_helpers.py
@@ -1,7 +1,10 @@
 """Tests for helper functions
 """
+import os
+
 from unittest import TestCase
 from ..helpers import stringify_credentials
+from ..helpers import parse_path
 from nose.tools import eq_
 
 
@@ -21,3 +24,47 @@ class TestHelpers(TestCase):
         """
         result = stringify_credentials('a', 'b', 'c')
         eq_(result, 'aws_access_key_id=a;aws_secret_access_key=b;token=c')
+
+    @staticmethod
+    def test_parse_path_none_returns_none():
+        """Tests that giving parse_path a None path returns None
+        """
+        eq_(parse_path(None), None)
+        eq_(parse_path(None, 'test'), None)
+
+    @staticmethod
+    def test_parse_path_abs_path_returns_itself():
+        """Tests that giving parse_path an absolute path returns itself
+        """
+        eq_(parse_path('/abs/path'), '/abs/path')
+
+    @staticmethod
+    def test_parse_path_relative_path_returns_new_path():
+        """Tests that a relative path gets transformed
+        """
+        from dataduct.config import Config
+        config = Config()
+        config.etl['TEST_PATH'] = '/transform'
+        eq_(parse_path('test/path', 'TEST_PATH'), '/transform/test/path')
+
+    @staticmethod
+    def test_parse_path_relative_path_no_matching_config_returns_itself():
+        """Tests that the original path is returned if no matching
+        transformation can be found
+        """
+        from dataduct.config import Config
+        config = Config()
+        config.etl.pop('TEST_PATH', None)
+        eq_(parse_path('test/path', 'TEST_PATH'), 'test/path')
+
+    @staticmethod
+    def test_parse_path_expands_user():
+        """Tests that parse_path expands the user symbol
+        """
+        from dataduct.config import Config
+        config = Config()
+        config.etl['TEST_PATH'] = '~/transform'
+        eq_(
+            parse_path('test/path', 'TEST_PATH'),
+            os.path.expanduser('~/transform/test/path'),
+        )
