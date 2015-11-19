538199cd745cfe87c9dd49e69f59504bdba98475
Sourabh Bajaj <sb2nov@gmail.com>
2015-11-19 15:11:54 +0530

    Support for argument propogation

--- dataduct/utils/constants.py
+++ dataduct/utils/constants.py
@@ -21,28 +21,32 @@ SRC_STR = 'src'
 QA_STR = 'qa'
 
 # Commands
-COUNT_CHECK_COMMAND = ('python -c "from dataduct.steps.executors.count_check '
-                       'import count_check; count_check()"')
+ARGUMENT_PROPOGATOR = ' "$@"'
+COUNT_CHECK_COMMAND = (
+    'python -c "from dataduct.steps.executors.count_check import '
+    'count_check; count_check()"') + ARGUMENT_PROPOGATOR
 
-COLUMN_CHECK_COMMAND = ('python -c "from dataduct.steps.executors.column_check'
-                        ' import column_check; column_check()"')
+COLUMN_CHECK_COMMAND = (
+    'python -c "from dataduct.steps.executors.column_check import '
+    'column_check; column_check()"') + ARGUMENT_PROPOGATOR
 
-LOAD_COMMAND = ('python -c "from '
-                'dataduct.steps.executors.create_load_redshift import '
-                'create_load_redshift_runner; create_load_redshift_runner()"')
+LOAD_COMMAND = (
+    'python -c "from dataduct.steps.executors.create_load_redshift import '
+    'create_load_redshift_runner; create_load_redshift_runner()"') +\
+    ARGUMENT_PROPOGATOR
 
-PK_CHECK_COMMAND = ('python -c "from '
-                    'dataduct.steps.executors.primary_key_check import '
-                    'primary_key_check; primary_key_check()"')
+PK_CHECK_COMMAND = (
+    'python -c "from dataduct.steps.executors.primary_key_check import '
+    'primary_key_check; primary_key_check()"') + ARGUMENT_PROPOGATOR
 
-DEPENDENCY_COMMAND = ('python -c "from '
-                      'dataduct.steps.executors.dependency_check import '
-                      'dependency_check; dependency_check()"')
+DEPENDENCY_COMMAND = (
+    'python -c "from dataduct.steps.executors.dependency_check import '
+    'dependency_check; dependency_check()"') + ARGUMENT_PROPOGATOR
 
-SCRIPT_RUNNER_COMMAND = ('python -c "from '
-                         'dataduct.steps.executors.runner import '
-                         'script_runner; script_runner()"')
+SCRIPT_RUNNER_COMMAND = (
+    'python -c "from dataduct.steps.executors.runner import '
+    'script_runner; script_runner()"') + ARGUMENT_PROPOGATOR
 
-SQL_RUNNER_COMMAND = ('python -c "from '
-                      'dataduct.steps.executors.runner import '
-                      'sql_runner; sql_runner()"')
+SQL_RUNNER_COMMAND = (
+    'python -c "from dataduct.steps.executors.runner import '
+    'sql_runner; sql_runner()"') + ARGUMENT_PROPOGATOR
