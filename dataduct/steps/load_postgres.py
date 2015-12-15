ca97f573f3291abf7108831d80102d05c3c5e578
anuragbhardwaj <anuragbhardwaj@quadanalytix.com>
2015-12-15 11:57:20 -0800

    Changes to load postgres interface

--- dataduct/steps/load_postgres.py
+++ dataduct/steps/load_postgres.py
@@ -19,12 +19,8 @@ class LoadPostgresStep(ETLStep):
         """Constructor for the LoadPostgresStep class
 
         Args:
-            schema(str): schema from which table should be extracted
             table(path): table name for extract
-            insert_mode(str): insert mode for redshift copy activity
-            redshift_database(RedshiftDatabase): database to excute the query
-            max_errors(int): Maximum number of errors to be ignored during load
-            replace_invalid_char(char): char to replace not utf-8 with
+            database(Database): database to excute the query
             **kwargs(optional): Keyword arguments directly passed to base class
         """
         super(LoadPostgresStep, self).__init__(**kwargs)
@@ -68,6 +64,6 @@ class LoadPostgresStep(ETLStep):
             step_args(dict): Dictionary of the step arguments for the class
         """
         step_args = cls.base_arguments_processor(etl, input_args)
-        step_args['redshift_database'] = etl.redshift_database
+        step_args['postgres_database'] = etl.postgres_database
 
         return step_args
