7738e61e179e4cc71f14cebc29b488982fe4dbb7
Abhishek Mahawar <amahawar@quadanalytix.com>
2016-03-04 10:42:44 +0530

    Added support for 15 minutes and 30 minutes frequency for pipeline

--- dataduct/pipeline/schedule.py
+++ dataduct/pipeline/schedule.py
@@ -37,6 +37,8 @@ FEQUENCY_PERIOD_CONVERTION = {
     '8-hours': ('8 hours', None),
     '12-hours': ('12 hours', None),
     'one-time': ('15 minutes', 1),
+    '30-min': ('30 minutes', None),
+    '15-min': ('15 minutes', None),
 }
 
 
