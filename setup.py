22ae24648ba186fddff1a2c11f7b84f6c390c990
Sourabh Bajaj <sb2nov@gmail.com>
2016-10-03 09:39:30 -0700

    Bump version for python 2.6

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         'boto>=2.38',
         'MySQL-python>=1.2.3',
-        'pandas==0.18.0',
+        'pandas==0.18.1',
         'psycopg2>=2.6',
         'pyparsing>=1.5.6',
         'pytimeparse>=1.1.4',
